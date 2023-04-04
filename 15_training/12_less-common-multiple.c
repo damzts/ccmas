1703da0732f07ee5b93bf0398541103109174ba4
damzts <davidmtztorres00@gmail.com>
2023-04-03 19:21:21 -0700

    minimo comun multiplo, from scratch, no help

--- 15_training/12_less-common-multiple.c
+++ 15_training/12_less-common-multiple.c
@@ -3,93 +3,161 @@
  *
  *       Filename:  12_less-common-multiple.c
  *
- *    Description:  
+ *    Description:
  *
  *        Version:  1.0
  *        Created:  04/03/2023 12:38:58 PM
  *       Revision:  none
  *       Compiler:  gcc
  *
- *         Author:  YOUR NAME (), 
- *   Organization:  
+ *         Author:  YOUR NAME (),
+ *   Organization:
  *
  * =====================================================================================
  */
 
-#include	<stdlib.h>
-#include    <stdio.h>
-int* getPrimeFactors(int);
-int getLCM(int,int);
-/* 
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct FactoresPrimos_t
+{
+    int *factoresPrimos; // puntero a un array
+    int len;             // longitud del array al que apunta
+} FactoresPrimos;
+
+FactoresPrimos *getPrimeFactors(int);
+int getLCM(int, int);
+/*
  * ===  FUNCTION  ======================================================================
  *         Name:  main
- *  Description:  
+ *  Description:
  * =====================================================================================
  */
-    int
-main ( int argc, char *argv[] )
+int main(int argc, char *argv[])
 {
-    //encontrar los factores primos de both numbers
-    //la union de ambos
-    //la multiplacion de la union
-    //el resultado
-//    int a=15,b=20;
-//    printf ( "el lcm de %d:%d es: %d\n",a,b,getLCM(a,b));
-  int* a =  getPrimeFactors(15);
+    // algoritmo
+    // descomponer ambos numeros en factores primos
+    // hacer la union de los factores primos
+    // multiplicar los factores primos
+    // el resultado
+    // getLCM(15, 20);
+    int a=15,b=20;
+    printf("\nEl lcm de %d:%d es: %d\n", a, b, getLCM(a, b));
     return EXIT_SUCCESS;
 }
-/* 
+/*
  * ===  FUNCTION  ======================================================================
  *         Name:  getPrimeNumbers
- *  Description:  
+ *  Description:
  * =====================================================================================
  */
-   int* 
-getPrimeFactors (int a)
+FactoresPrimos_t *getPrimeFactors(int a)
 {
-    int primeNumbersArray[10];
-    static int primeFactorsArray;
-    int i;
-    int counter=0;
-    //primero encontrar los numeros primos del 2 al a
-    for ( i = 2; i < a; i += 1 ) {
-    
-        for (int j = 2; j < i; j += 1 ) {
-            //si el numero tiene divisores
-            if(i%j == 0){
-                //no es numero primo
+    int resultn = a;
+    int arrayFactor[10];
+    int counter = 0;
+    // decomposing
+    while (resultn != 1)
+    {
+        for (int i = 2; i < a; i++)
+        {
+            if (resultn % i == 0)
+            {
+                int factor = i;
+                // printf("%d,", factor);
+                resultn = resultn / factor;
+                arrayFactor[counter] = factor;
+                counter++;
                 break;
             }
         }
-        //        printf ( "%d,",i );
-        while(counter<10){
-            primeNumbersArray[counter]=i;
+    }
+    // printf("entre a get prime factors\n");
+    FactoresPrimos *f = (FactoresPrimos *)malloc(sizeof(FactoresPrimos));
+    f->len = counter;
+    int *arrayFactorRezided = (int *)malloc(sizeof(int) * counter);
+    for (int j = 0; j < counter; j++)
+    {
+        arrayFactorRezided[j] = arrayFactor[j];
+        printf("%d:", arrayFactor[j]);
+    }
+    f->factoresPrimos = arrayFactorRezided;
+    printf("\n");
+    return f;
+}
+/*
+ * ===  FUNCTION  ======================================================================
+ *         Name:  getLCM
+ *  Description:
+ * =====================================================================================
+ */
+int getLCM(int a, int b)
+{
+    FactoresPrimos *arrayA = getPrimeFactors(a); // puntero a un array
+    FactoresPrimos *arrayB = getPrimeFactors(b); // puntero a un array
+    printf("nomames\n");
+    // printf("\n%d\n",array->len);
+    // printf("\n%d\n", array->factoresPrimos[2]);
+    // 1.hacer la union
+    // 5 5 : 5 3 = 5 5 3
+    // 5 3 : 5 2 2 = 5 3 2 2
+    int arrayUnion[20];
+    int counter = 0;
+
+    for (int indexArrayA = 0; indexArrayA < arrayA->len; indexArrayA++)
+    {
+        int notFound = 1;
+        // pivot A
+        for (int indexArrayB = 0; indexArrayB < arrayB->len; indexArrayB++)
+        {
+            if (arrayA->factoresPrimos[indexArrayA] == arrayB->factoresPrimos[indexArrayB])
+            {
+                arrayUnion[counter] = arrayA->factoresPrimos[indexArrayA];
+                counter++;
+                arrayA->factoresPrimos[indexArrayA] = 1;
+                arrayB->factoresPrimos[indexArrayB] = 1;
+                notFound = 0;
+                break;
+            }
+        }
+
+        if (notFound == 1)
+        {
+            arrayUnion[counter] = arrayA->factoresPrimos[indexArrayA];
             counter++;
         }
     }
-    for(int k=0;k<10;k++){
-        if(a%primeNumbersArray[k]==0){
 
+    for (int indexArrayB2 = 0; indexArrayB2 < arrayB->len; indexArrayB2++)
+    {
+        int notFound = 1;
+        // pivot B
+        for (int indexArrayA2 = 0; indexArrayA2 < arrayA->len; indexArrayA2++)
+        {
+            if (arrayB->factoresPrimos[indexArrayB2] == arrayA->factoresPrimos[indexArrayA2])
+            {
+                arrayUnion[counter] = arrayB->factoresPrimos[indexArrayB2];
+                counter++;
+                arrayB->factoresPrimos[indexArrayB2] = 1;
+                arrayA->factoresPrimos[indexArrayA2] = 1;
+                notFound = 0;
+                break;
+            }
         }
-        printf("%d,",primeNumbersArray[k]);
+        if (notFound == 1)
+        {
+            arrayUnion[counter] = arrayB->factoresPrimos[indexArrayB2];
+            counter++;
+        }
     }
-    return primeNumbersArray;
-}
-/* 
- * ===  FUNCTION  ======================================================================
- *         Name:  getLCM
- *  Description:  
- * =====================================================================================
- */
-   int 
-getLCM (int a, int b )
-{
-    int lcm=10;
-    int* aPrimeFactors=getPrimeFactors(a);
-    int* bPrimeFactors=getPrimeFactors(b);
 
-
-
+    // multiplicar el array resultante
+    int lcm = 1;
+    for (int indexArrayUnion = 0; indexArrayUnion < counter; indexArrayUnion++)
+    {
+        printf("%d:", arrayUnion[indexArrayUnion]);
+        lcm = lcm * arrayUnion[indexArrayUnion];
+    }
 
     return lcm;
 }
